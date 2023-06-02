ff07df378177b9058d081199c17b2313fe0f6518
AbdoSalama99 <bdhhlalslamh@gmail.com>
2023-06-02 05:26:54 -0700

    main commit

---
+++ script.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const currencyEl_one = document.getElementById('currency-one');
+const amountEl_one = document.getElementById('amount-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_two = document.getElementById('amount-two');
+const swapEl = document.getElementById('swap');
+const rateEl = document.getElementById('rate');
+
+///calculate function
+const calculate = function () {
+    const currency_one = currencyEl_one.value;
+    const currency_two = currencyEl_two.value;
+    fetch(`https://open.exchangerate-api.com/v6/latest/${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            const rate = data.rates[currency_two];
+            rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+            amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+        })
+
+}
+
+
+calculate();
+////event listiners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+swapEl.addEventListener('click', () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    calculate();
+})
